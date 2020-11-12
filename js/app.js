515acf3a1923bf11d5dc991218d4225147c6268c
Armando Arce <armando.arce@gmail.com>
2020-11-12 11:41:25 -0600

    Update

---
+++ js/app.js
@@ -0,0 +1,59 @@
+function getAll(entity) {
+	fetch('api/' + entity)
+		.then((data) => {
+			fetch('/template/list/' + entity + '.html')
+				.then((response) => response.text())
+				.then((template) => {
+					var rendered = Mustache.render(template, data);
+					document.getElementById('content').innerHTML = rendered;
+				});
+		})
+}
+
+function getById(query, entity) {
+	var params = new URLSearchParams(query);
+	fetch('api/' + entity + '/' + params.get('id'))
+		.then((data) => {
+			fetch('/template/detail/' + entity + '.html')
+				.then((response) => response.text())
+				.then((template) => {
+					var rendered = Mustache.render(template, data);
+					document.getElementById('content').innerHTML = rendered;
+				});
+		})
+}
+
+function home() {
+	fetch('/template/home.html')
+		.then((response) => response.text())
+		.then((template) => {
+			var rendered = Mustache.render(template, {});
+			document.getElementById('content').innerHTML = rendered;
+		});
+}
+
+function init() {
+	router = new Navigo(null, false, '#!');
+	router.on({
+		'/books': function() {
+			getAll('books');
+		},
+		'/authors': function() {
+			getAll('authors');
+		},
+		'/publishers': function() {
+			getAll('publishers');
+		},
+		'/bookById': function(_, query) {
+			getById(query, 'books');
+		},
+		'/authorById': function(_, query) {
+			getById(query, 'authors');
+		},
+		'/publisherById': function(_, query) {
+			getById(query, 'publishers');
+		}
+	});
+	router.on(() => home());
+	router.resolve();
+}
